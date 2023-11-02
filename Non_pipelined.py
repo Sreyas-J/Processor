eff546e7067baf4b791977740c8728cc22dc7d73
PradyumnaG <Pradyumna.G@iiitb.ac.in>
2023-11-03 02:11:47 +0530

    code complete with errors

--- Non_pipelined.py
+++ Non_pipelined.py
@@ -1,8 +1,9 @@
 import os
 
+# instr_begg = 2**20
 pc=0
 mem=[]
-file_path = "RealOutput.asm" 
+file_path = "Fake_Output.txt" 
 dataMem=[]
 regMem = []
 
@@ -25,24 +26,43 @@ def decimal_to_binary(number, num_bits):
     return binary_with_padding
 
 def binary_to_decimal(number_str):
+    if(len(number_str)>30 and number_str[30:32]=="b1"):
+        number_str =number_str[0:29]+ "01"
+    # print(number_str)
     number = int(number_str,2)
+    # print(number)
     return number
 
 
-
 for i in range(32):
-    regMem.append(0)
-# regMem[9] =
-# regMem[10] = 
-# regMem[11] = 
+    regMem.append("0"*32)
 
-for i in range(mem.len()):
+regMem[0] = decimal_to_binary(0,32)
+regMem[9] = decimal_to_binary(3,32)
+regMem[10] = decimal_to_binary(0,32)
+regMem[11] = decimal_to_binary(5,32)
+
+for i in range(50):
+    dataMem.append("0"*32)
+
+dataMem[0] = decimal_to_binary(13,32)
+dataMem[1] = decimal_to_binary(7,32)
+dataMem[2] = decimal_to_binary(10,32)
+
+while(pc<len(mem)):
+    print()
     op=mem[pc][0:6]
+    # print(regMem[23])
 
     if(op=="000000"):
+        
+        # print(mem[pc][16:21])
         rs=binary_to_decimal(mem[pc][6:11])
         rt=binary_to_decimal(mem[pc][11:16])
         rd=binary_to_decimal(mem[pc][16:21])
+        # print("HI")
+        # print(regMem[rd])
+        # print(rd)
 
         HI=""
         LO=""
@@ -63,13 +83,13 @@ for i in range(mem.len()):
             regMem = LO
 
     elif(op=="000010" or op=="000011"):
-        j=mem[pc][6:32]
-        
+        j=mem[pc][6:32] + "00"
+        pc = binary_to_decimal(j)
 
     else:
         rs=binary_to_decimal(mem[pc][6:11])
         rt=binary_to_decimal(mem[pc][11:16])
-        imm=binary_to_decimal(mem[pc][16:32])
+        imm=mem[pc][16:32]
 
         if(op==decimal_to_binary(35,6)):
             regMem[rt] = dataMem[binary_to_decimal(regMem[rs])+binary_to_decimal(imm)]
@@ -81,10 +101,20 @@ for i in range(mem.len()):
             regMem[rt][0:16] = imm
         elif(op==decimal_to_binary(13,6)):
             regMem[rs] = decimal_to_binary(binary_to_decimal(regMem[rt]) | binary_to_decimal(imm),32)
-        
-
-
-    if(op=="001001"):
-        pass
+        elif(op==decimal_to_binary(4,6)):
+            # print(rt)
+            if(binary_to_decimal(regMem[rs])==binary_to_decimal(regMem[rt])):
+                pc = pc + 1 + binary_to_decimal(imm)
+        elif(op==decimal_to_binary(7,6)):
+            if(binary_to_decimal(regMem[rs])>binary_to_decimal(regMem[rt])):
+                pc = pc + 1 + binary_to_decimal(imm)
 
     pc+=1
+
+# output = [dataMem[5],dataMem[6],dataMem[7]]
+print("RegMem")
+print(regMem)
+print()
+print()
+print("DataMem")
+print(dataMem)
---
+++ p.py
@@ -0,0 +1,3 @@
+number_str = "00000000000000001111111111100000"
+number = int(number_str,2)
+print(number)
