62224221c9e019a0b3425a0d82d4807d0038e372
PradyumnaG <Pradyumna.G@iiitb.ac.in>
2023-11-02 23:46:47 +0530

    Few_intructions_added

--- p.py
+++
@@ -1,7 +0,0 @@
-number_str = "0101011"  # Replace this with the binary number string
-try:
-    number = int(number_str, 2)  # Convert binary string to an integer
-    print(type(number))
-    print(f"The integer representation of {number_str} is: {number}")
-except ValueError:
-    print("Invalid input. Please provide a valid binary string.")
